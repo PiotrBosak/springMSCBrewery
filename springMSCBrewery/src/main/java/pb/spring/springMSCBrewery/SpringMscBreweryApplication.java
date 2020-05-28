4ea3b3eeaa9ab8522a8ab054986f667e771cada7
piotr <PiotrBosak@yahoo.com>
2020-05-28 13:06:04 +0200

    init

---
+++ springMSCBrewery/src/main/java/pb/spring/springMSCBrewery/SpringMscBreweryApplication.java
@@ -0,0 +1,13 @@
+package pb.spring.springMSCBrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMscBreweryApplication.class, args);
+	}
+
+}
---
+++ springMSCBrewery/src/test/java/pb/spring/springMSCBrewery/SpringMscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.spring.springMSCBrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
